ccd857b031cad2deb2642370df8d9f37efebb725
Margarita Staykova <stm4394@gmail.com>
2022-04-07 23:02:03 +0300

    First Steps In Coding

---
+++ First Steps In Coding - Lab/08. Pet Shop/08. Pet Shop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Pet_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double packetsOfFoodForDogs = double.Parse(Console.ReadLine());
+            int packetsOfFoodForCats = int.Parse(Console.ReadLine());
+            double sumForDogs = packetsOfFoodForDogs * 2.50;
+            int sumForCats = packetsOfFoodForCats * 4;
+            Console.WriteLine(sumForCats + sumForDogs + " lv.");
+        }
+    }
+}
---
+++ First Steps In Coding - Lab/09. Yard Greening/09. Yard Greening/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Yard_Greening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double squareMetres = double.Parse(Console.ReadLine());
+            double price = squareMetres * 7.61;
+            double discount = price * 0.18;
+            double finalPrice = price - discount;
+            Console.WriteLine($"The final price is: {finalPrice} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
