55da2f414006ce56d4c4717b90a038ba20f25f38
Margarita Staykova <stm4394@gmail.com>
2022-04-09 11:34:11 +0300

    Added

---
+++ SoftUni Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/04. Back In 30 Minutes/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Back_In_30_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            if (minutes > 60)
+            {
+                minutes = minutes - 60;
+                hours += 1;
+            }
+
+            if (hours == 24)
+            {
+                hours = 0;
+            }
+
+            if (minutes == 60)
+            {
+                minutes = 0;
+            }
+
+            Console.WriteLine($"{hours}:{minutes:D2}");
+        }
+    }
+}
