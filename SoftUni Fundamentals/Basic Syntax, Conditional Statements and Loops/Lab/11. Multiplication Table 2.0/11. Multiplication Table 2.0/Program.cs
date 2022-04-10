ca991bf251cd3db482ad64b8da27952edfc38576
Margarita Staykova <stm4394@gmail.com>
2022-04-10 12:29:27 +0300

    Added

---
+++ SoftUni Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/11. Multiplication Table 2.0/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Multiplication_Table_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var multiplier = int.Parse(Console.ReadLine());
+
+            if (multiplier > 10)
+            {
+                Console.WriteLine($"{n} X {multiplier} = {n * multiplier}");
+            }
+            else
+            {
+                for (int i = multiplier; i <= 10; i++)
+                {
+                    Console.WriteLine($"{n} X {i} = {n * i}");
+                }
+            }
+        }
+    }
+}
