cd45de090b846ee37535b17b94378572bb8781fe
Margarita Staykova <stm4394@gmail.com>
2022-04-10 12:32:48 +0300

    Add

---
+++ SoftUni Fundamentals/Basic Syntax, Conditional Statements and Loops/Exercise/01. Ages/01. Ages/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Ages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 0 && age <= 2)
+            {
+                Console.WriteLine("baby");
+            }
+            if (age >= 3 && age <= 13)
+            {
+                Console.WriteLine("child");
+            }
+            if (age >= 14 && age <= 19)
+            {
+                Console.WriteLine("teenager");
+            }
+            if (age >= 20 && age <= 65)
+            {
+                Console.WriteLine("adult");
+            }
+            if (age >= 66)
+            {
+                Console.WriteLine("elder");
+            }
+        }
+    }
+}
---
+++ SoftUni Fundamentals/Basic Syntax, Conditional Statements and Loops/Exercise/03. Vacation/03. Vacation/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double AmountOfPeople = double.Parse(Console.ReadLine());
+            string Group = Console.ReadLine();
+            string Day = Console.ReadLine();
+
+            if (Group == "Students")
+            {
+                double price = 0;
+                if (Day == "Friday")
+                {
+                    price = 8.45;
+                }
+                else if (Day == "Saturday")
+                {
+                    price = 9.80;
+                }
+                else if (Day == "Sunday")
+                {
+                    price = 10.46;
+                }
+
+                if (AmountOfPeople >= 30)
+                {
+                    double fee = price * AmountOfPeople;
+                    double discount = fee * 0.15;
+                    double finalPrice = fee - discount;
+                    Console.WriteLine($"Total price: {finalPrice:F2}");
+                }
+                else
+                {
+                    double PriceWithoutDiscount = price * AmountOfPeople;
+                    Console.WriteLine($"Total price: {PriceWithoutDiscount:F2}");
+                }
+            }
+            else if (Group == "Business")
+            {
+                double price = 0;
+                if (Day == "Friday")
+                {
+                    price = 10.90;
+                }
+                else if (Day == "Saturday")
+                {
+                    price = 15.60;
+                }
+                else if (Day == "Sunday")
+                {
+                    price = 16;
+                }
+
+                if (AmountOfPeople >= 100)
+                {
+                    double fee = price * (AmountOfPeople - 10);
+                    Console.WriteLine($"Total price: {fee:F2}");
+                }
+                else
+                {
+                    double PriceWithoutDiscount = price * AmountOfPeople;
+                    Console.WriteLine($"Total price: {PriceWithoutDiscount:F2}");
+                }
+            }
+            else if (Group == "Regular")
+            {
+                double price = 0;
+                if (Day == "Friday")
+                {
+                    price = 15;
+                }
+                else if (Day == "Saturday")
+                {
+                    price = 20;
+                }
+                else if (Day == "Sunday")
+                {
+                    price = 22.50;
+                }
+
+                if (AmountOfPeople >= 10 && AmountOfPeople <= 20)
+                {
+                    double fee = price * AmountOfPeople;
+                    double discount = fee * 0.05;
+                    double finalPrice = fee - discount;
+                    Console.WriteLine($"Total price: {finalPrice:F2}");
+                }
+                else
+                {
+                    double PriceWithoutDiscount = price * AmountOfPeople;
+                    Console.WriteLine($"Total price: {PriceWithoutDiscount:F2}");
+                }
+            }
+
+        }
+    }
+}
