b2bf55706f96619531f530b6e8c94f218f1a0d0d
Margarita Staykova <stm4394@gmail.com>
2022-04-10 12:26:09 +0300

    Added

---
+++ SoftUni Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/09. Sum of Odd Numbers/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Sum_of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var oddSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentOddNumber = 1 + (2 * i);
+                Console.WriteLine(currentOddNumber);
+                oddSum += currentOddNumber;
+            }
+
+            Console.WriteLine($"Sum: {oddSum}");
+        }
+    }
+}
