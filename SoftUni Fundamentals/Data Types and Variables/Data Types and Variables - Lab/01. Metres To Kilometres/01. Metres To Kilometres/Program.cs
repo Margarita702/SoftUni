6c2061b8a227d86605ce640f7a57da2244716e37
Margarita Staykova <stm4394@gmail.com>
2022-04-11 14:27:01 +0300

    Added

---
+++ SoftUni Fundamentals/Data Types and Variables/Data Types and Variables - Lab/01. Metres To Kilometres/01. Metres To Kilometres/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Metres_To_Kilometres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+            double kilometeres = meters / 1000.0;
+            Console.WriteLine($"{Math.Round(kilometeres, 2, MidpointRounding.AwayFromZero):F2}");
+        }
+    }
+}
