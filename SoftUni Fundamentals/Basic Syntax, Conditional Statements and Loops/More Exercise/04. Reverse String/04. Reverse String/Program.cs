ef4b9c6d9da9e8ab7d0ad7f8e7ebe84018c01303
Margarita Staykova <stm4394@gmail.com>
2022-04-11 14:19:25 +0300

    Added

---
+++ SoftUni Fundamentals/Basic Syntax, Conditional Statements and Loops/More Exercise/04. Reverse String/04. Reverse String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string originalString = Console.ReadLine();
+            string reverseString = string.Empty;
+            for (int i = originalString.Length - 1; i >= 0; i--)
+            {
+                reverseString += originalString[i];
+            }
+            Console.Write(reverseString);
+            Console.ReadLine();
+        }
+    }
+}
