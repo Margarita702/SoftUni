a95bf26f8cc335b14d1f7d3561a3150e51255bbb
Margarita Staykova <stm4394@gmail.com>
2022-04-24 20:04:05 +0300

    SoftUni

---
+++ SoftUni Advanced/Stacks and Queues - Exercise/10. Crossroads/10. Crossroads/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Crossroads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenLight = int.Parse(Console.ReadLine());
+            int freeWndow = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int carpassed = 0;
+      
+            Queue<string> cars = new Queue<string>();
+            while (input!="END")
+            {
+                if (input!="green")
+                {
+                    cars.Enqueue(input);
+                    input = Console.ReadLine();
+                    continue;
+                }
+                int currGreenLight = greenLight;
+
+                while (currGreenLight > 0 && cars.Count > 0)
+                {
+                    string currCar = cars.Dequeue();
+                    if (currGreenLight - currCar.Length >= 0)
+                    {
+                        currGreenLight-=currCar.Length;
+                        carpassed++;
+                        continue;
+                    }
+                    if (currGreenLight + freeWndow - currCar.Length >= 0)
+                    {
+                        currGreenLight = 0;
+                        carpassed++;
+                        continue;
+                    }
+
+                    int hittedChar = currGreenLight + freeWndow;
+                    Console.WriteLine("A crash happened!");
+                    Console.WriteLine($"{currCar} was hit at {currCar[hittedChar]}.");
+                    Environment.Exit(0);
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{carpassed} total cars passed the crossroads.");
+        }
+    }
+}
---
+++ SoftUni Advanced/Stacks and Queues - Exercise/11. Key Revolver/11. Key Revolver/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Key_Revolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
