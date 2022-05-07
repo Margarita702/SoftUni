26e36d3cc6385227b27170e0ef81fc1476fba000
Margarita Staykova <stm4394@gmail.com>
2022-05-07 20:42:01 +0300

    Added

--- SoftUni Advanced/Stacks and Queues/Stacks and Queues - Exercise/10. Crossroads/10. Crossroads/Program.cs
+++ SoftUni Advanced/Stacks and Queues/Stacks and Queues - Exercise/10. Crossroads/10. Crossroads/Program.cs
@@ -10,6 +10,47 @@ namespace _10.Crossroads
     {
         static void Main(string[] args)
         {
+            int greenLight = int.Parse(Console.ReadLine());
+            int freeWndow = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int carpassed = 0;
+
+            Queue<string> cars = new Queue<string>();
+            while (input != "END")
+            {
+                if (input != "green")
+                {
+                    cars.Enqueue(input);
+                    input = Console.ReadLine();
+                    continue;
+                }
+                int currGreenLight = greenLight;
+
+                while (currGreenLight > 0 && cars.Count > 0)
+                {
+                    string currCar = cars.Dequeue();
+                    if (currGreenLight - currCar.Length >= 0)
+                    {
+                        currGreenLight -= currCar.Length;
+                        carpassed++;
+                        continue;
+                    }
+                    if (currGreenLight + freeWndow - currCar.Length >= 0)
+                    {
+                        currGreenLight = 0;
+                        carpassed++;
+                        continue;
+                    }
+
+                    int hittedChar = currGreenLight + freeWndow;
+                    Console.WriteLine("A crash happened!");
+                    Console.WriteLine($"{currCar} was hit at {currCar[hittedChar]}.");
+                    Environment.Exit(0);
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{carpassed} total cars passed the crossroads.");
         }
     }
 }
