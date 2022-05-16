1df446d0f983452c32628049f66d171a8e006bcd
Margarita Staykova <stm4394@gmail.com>
2022-05-16 22:41:39 +0300

    Add

---
+++ SoftUni Advanced/Multidimensional Arrays/Multidimensional Arrays - Lab/5. Square With Maximum Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Square_With_Maximum_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SoftUni Advanced/Multidimensional Arrays/Multidimensional Arrays - Lab/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
