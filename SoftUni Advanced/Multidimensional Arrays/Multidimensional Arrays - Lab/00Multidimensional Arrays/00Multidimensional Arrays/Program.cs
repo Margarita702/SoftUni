6fc47218ea59e52889ff4360eb73ef6d35285789
Margarita Staykova <stm4394@gmail.com>
2022-05-20 23:28:04 +0300

    Add

--- SoftUni Advanced/Multidimensional Arrays/Multidimensional Arrays - Lab/00Multidimensional Arrays/00Multidimensional Arrays/Program.cs
+++ SoftUni Advanced/Multidimensional Arrays/Multidimensional Arrays - Lab/00Multidimensional Arrays/00Multidimensional Arrays/Program.cs
@@ -10,6 +10,19 @@ namespace _00Multidimensional_Arrays
     {
         static void Main(string[] args)
         {
+            int[,] numbers = { { 2, 3, 9 }, { 4, 5, 9 } };
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                Console.Write("Row " + i + ": ");
+
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    Console.Write(numbers[i, j] + " ");
+
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- SoftUni Advanced/Multidimensional Arrays/Multidimensional Arrays - Lab/00Multidimensional Arrays/3. Primary Diagonal/Program.cs
+++ SoftUni Advanced/Multidimensional Arrays/Multidimensional Arrays - Lab/00Multidimensional Arrays/3. Primary Diagonal/Program.cs
@@ -10,6 +10,27 @@ namespace _3.Primary_Diagonal
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            int sum = 0;
+            for (int roww = 0; roww < matrix.GetLength(0); roww++)
+            {
+                sum += matrix[roww, roww];
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
