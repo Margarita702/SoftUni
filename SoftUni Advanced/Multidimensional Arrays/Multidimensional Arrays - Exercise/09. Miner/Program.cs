3516da6b6ceb8b9d73a5cf67618500c3d086382a
Margarita Staykova <stm4394@gmail.com>
2022-05-20 23:59:07 +0300

    Added

---
+++ SoftUni Advanced/Multidimensional Arrays/Multidimensional Arrays - Exercise/09. Miner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Miner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
