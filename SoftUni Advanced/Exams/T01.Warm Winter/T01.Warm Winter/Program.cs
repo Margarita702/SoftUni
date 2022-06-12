686946d3d0be42e00976825f68543c236397d781
Margarita Staykova <stm4394@gmail.com>
2022-06-13 00:05:51 +0300

    Add

---
+++ SoftUni Advanced/Exams/T01.Warm Winter/T01.Warm Winter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T01.Warm_Winter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] hats = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] scarfs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> hat = new Stack<int>(scarfs);
+            Queue <int> scarf = new Queue<int>(hats);
+           
+            List<int> sets = new List<int>();
+
+            while (hat.Any() && scarf.Any())
+            {         
+           
+                int hatt=hat.Peek();
+                int scarff = scarf.Peek();
+                if (hatt>scarff)
+                {
+                    
+                    sets.Add(hatt+scarff);
+                    hat.Pop();
+                    scarf.Dequeue();
+                   
+                }
+                else if (hatt < scarff)
+                {
+                    hat.Pop();
+                }
+                else
+                {
+                    scarf.Dequeue();
+                    hat.Push(hat.Pop() + 1);
+                }
+            }
+            Console.WriteLine($"The most expensive set is: {sets.Max()}");
+            Console.WriteLine(string.Join(" ", sets));
+        }
+    }
+}
---
+++ SoftUni Advanced/Streams, Files and Directories/Streams, Files and Directories - Exercises/T05. Copy Directory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T05.Copy_Directory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
