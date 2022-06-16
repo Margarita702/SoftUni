cc928587ff853a3589dd4c3a8a3dc356a6ec1efe
Margarita Staykova <stm4394@gmail.com>
2022-06-16 14:41:45 +0300

    Add

--- SoftUni Advanced/Exams/T01. Scheduling/T01. Scheduling/Program.cs
+++ SoftUni Advanced/Exams/T01. Scheduling/T01. Scheduling/Program.cs
@@ -10,6 +10,27 @@ namespace T01.Scheduling
     {
         static void Main(string[] args)
         {
+            var tasks = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse));
+            var threads = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            var taskToBeKilled = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                if (tasks.Peek()==taskToBeKilled)
+                {
+                    break;
+                }
+                if (tasks.Peek()<=threads.Peek())
+                {
+                    tasks.Pop();
+                    threads.Dequeue();
+                }
+                else
+                {
+                    threads.Dequeue();
+                }
+            }
+            Console.WriteLine($"Thread with value {threads.Peek()} killed task {taskToBeKilled}");
+            Console.WriteLine(string.Join(" ",threads));
         }
     }
 }
---
+++ SoftUni Advanced/Exams/T01.Flower Wreaths/T01.Flower Wreaths/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T01.Flower_Wreaths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lilies = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse));
+            var roses = new Queue<int>(Console.ReadLine().Split(", ").Select(int.Parse));
+            int wreaths = 0;
+            int flowers = 0;
+            while (roses.Any() && lilies.Any())
+            {
+                var lilie = lilies.Pop();
+                var rose = roses.Dequeue();
+                int sum = lilie + rose;
+                if (sum == 15)
+                {
+                    wreaths++;
+                }
+                
+                else if (sum < 15)
+                {
+                    flowers += sum;
+                }
+                else 
+                {
+                    while (sum > 15)
+                    {
+                        sum -= 2;
+                        if (sum == 15)
+                        {
+                            wreaths++;
+                            break;
+                        }
+                        else if (sum < 15)
+                        {
+                            flowers += sum;
+                            break;
+                        }
+                    }
+
+                }
+            }
+            wreaths += flowers / 15;
+            if (wreaths >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreaths} wreaths more!");
+            }
+        }
+    }
+}
