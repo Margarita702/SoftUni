36ed00a2ab61612f5f8b6efe67c112f2bef22ee2
Margarita Staykova <stm4394@gmail.com>
2022-07-03 16:29:47 +0300

    New Course added

---
+++ SoftUni Advanced/Exams/T02.Garden/T02.Garden/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace T02.Garden
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int armour = int.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+            char[][] field = new char[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                var chars = Console.ReadLine().ToCharArray();
+                field[i] = chars;
+            }
+            var heroRow = 0;
+            var heroCol = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < field[i].Length; j++)
+                {
+                    if (field[i][j] == 'A')
+                    {
+                        heroRow = i;
+                        heroCol = j;
+                    }
+                }
+
+            }
+            while (true)
+            {
+                var commansLine = Console.ReadLine();
+                var commandParts = commansLine.Split(' ');
+                var command = commandParts[0];
+                var orcRow = int.Parse(commandParts[1]);
+                var orcCol = int.Parse(commandParts[2]);
+                armour--;
+                field[orcRow][orcCol] = 'O';
+                field[heroRow][heroCol] = '-';
+                if (command == "up" && heroRow - 1 >= 0)
+                {
+                    heroRow--;
+                }
+                else if (command == "down" && heroRow + 1 < rows)
+                {
+                    heroRow++;
+                }
+                else if (command == "left" && heroCol - 1 >= 0)
+                {
+                    heroCol--;
+                }
+                else if (command == "right" && heroCol + 1 < field[heroRow].Length)
+                {
+                    heroCol++;
+                }
+
+                if (field[heroRow][heroCol] == 'O')
+                {
+                    armour -= 2;
+                }
+                else if (field[heroRow][heroCol] == 'M')
+                {
+                    field[heroRow][heroCol] = '-';
+                    Console.WriteLine($"The army managed to free the Middle World! Armor left: {armour}");
+                    break;
+                }
+
+                if (armour <= 0)
+                {
+                    field[heroRow][heroCol] = 'X';
+Console.WriteLine($"The army was defeated at {heroRow};{heroCol}.");
+                    break;
+                }
+                field[heroRow][heroCol] = 'A';
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < field[i].Length; j++)
+                {
+                    Console.Write(field[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
